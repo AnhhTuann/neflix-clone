41c5e47847afa22afc5fbad8d461671de371458b
AnhhTuann <tuannguyen25101999@gmail.com>
2021-09-27 19:17:47 +0700

    xong data

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,19 +1,18 @@
 // Initialize Cloud Firestore through Firebase
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { seedDatabase } from "../seed";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAlu3ckJ_NeIoUj8HgPlpOEjc06L_3uLBE",
-  authDomain: "netflix-clone-7e8c2.firebaseapp.com",
-  projectId: "netflix-clone-7e8c2",
-  storageBucket: "netflix-clone-7e8c2.appspot.com",
-  messagingSenderId: "4610464854",
-  appId: "1:4610464854:web:34bcc3310378c112446ff7",
+  apiKey: "AIzaSyDJD_QkcBVRNorxs5DgxwtpbUKqkngdkHI",
+  authDomain: "netflix-clone-7a099.firebaseapp.com",
+  projectId: "netflix-clone-7a099",
+  storageBucket: "netflix-clone-7a099.appspot.com",
+  messagingSenderId: "488928943570",
+  appId: "1:488928943570:web:73f1e0f125576a55d4fc0b",
+  measurementId: "G-GJYKB2QG1Q",
 });
 
 export const db = getFirestore();
-
 seedDatabase(firebaseApp);
-
 export { firebaseApp };
