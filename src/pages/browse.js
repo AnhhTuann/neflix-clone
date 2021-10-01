c742295e7c79059fa3db9a0cf3f96695c0666f63
AnhhTuann <tuannguyen25101999@gmail.com>
2021-10-01 21:40:44 +0700

    filter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,14 +1,15 @@
 import { useContent } from "../hooks";
-// import { selectionFilter } from "../utils";
+import { selectionFilter } from "../utils";
 
 export default function Browse() {
   const series = useContent("series");
   const films = useContent("films");
 
-  console.log(series);
-  console.log(films);
-  // const slides = selectionFilter({ series, films });
-  // console.log(slides);
+  // console.log(series);
+  // console.log(films);
+
+  const slides = selectionFilter(series.series, films.films);
+  console.log(slides);
 
   return <p>Day la Browser</p>;
 }
